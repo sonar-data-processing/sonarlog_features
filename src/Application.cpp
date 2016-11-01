18d7a3d70c6a71ef2bfab2a5249d7cc9c1cdd74e
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2016-11-01 17:13:40 -0300

    roi and clahe

--- src/Application.cpp
+++ src/Application.cpp
@@ -6,6 +6,7 @@
 #include "rock_util/Utilities.hpp"
 #include "sonarlog_slam/Application.hpp"
 #include "sonar_processing/ImageUtils.hpp"
+#include "sonar_processing/Preprocessing.hpp"
 
 using namespace sonar_processing;
 
@@ -35,8 +36,22 @@ void Application::process_next_sample() {
     float angle = bearings[bearings.size()-1];
     uint32_t frame_height = 500;
     uint32_t frame_width = base::MathUtil::aspect_ratio_width(angle, frame_height);
-    cv::Mat src = sonar_util::Converter::convert2polar(sample.bins, bearings, sample.bin_count, sample.beam_count, frame_width, frame_height);
+    cv::Mat input = sonar_util::Converter::convert2polar(sample.bins, bearings, sample.bin_count, sample.beam_count, frame_width, frame_height);
+    cv::imshow("input", input);
+
+    /* roi frame */
+    cv::Mat src(sample.beam_count, sample.bin_count, CV_32F, (void*) sample.bins.data());
+    cv::Mat roi = src(preprocessing::calc_horiz_roi(src));
+    roi.convertTo(roi, CV_8U, 255);
+
+    /* image enhancement */
+    cv::Mat enhanced;
+    preprocessing::adaptative_clahe(roi, enhanced);
+
     cv::imshow("src", src);
+    cv::imshow("roi", roi);
+    cv::imshow("enhanced", enhanced);
+
     cv::waitKey(100);
 }
 
